d19d3b57610900d2864b543de8fe4ae18241ffca
Joel <a12022@ae4outubro.pt>
2024-11-22 09:51:08 +0000

    I continued to work on exercise 7

--- NOVEMBER/19 NOV/exercise seven.c
+++ NOVEMBER/19 NOV/exercise seven.c
@@ -52,5 +52,54 @@ int main() {
     }
     printf("\n\n");
 
+    int melhor_nota = 0;
+    for (int i = 0; i < 5; i++)
+        for (int x = 0; x < 2; x++)
+            if (notas[x][i] > melhor_nota)
+                melhor_nota = notas[x][i];
+        printf("a melhor nota foi: %d\n", melhor_nota);
+
+    printf("\n");
+
+
+    int idade [] = {17, 17, 16, 18, 16};
+    char *alunos [] = {"ANA", "CARLOS", "MANUEL", "RUI", "TIAGO"};
+
+    int opcao;
+
+    do {
+        printf("\n1. media de idades\n");
+        printf("2. nome dos alunos mais novos\n");
+        printf("3. mumero total de alunos com idades superiores ou iguais a 18\n");
+        printf("4. listagem dos nomes e respetiva idade\n");
+        printf("0. sair\n");
+        printf("opcao: ");
+        scanf("%d", &opcao);
+
+        if (opcao == 1) {
+            int total = 0;
+            for (int i = 0; i < 5; i++)
+                total = total + idade[i];
+            printf("media: %.2f\n", total / 5.0);
+        } else if (opcao == 2) {
+            int menor_idade = 0;
+            for (int i = 0; i < 5; i++) {
+                if (menor_idade > idade[i])
+                menor_idade = idade[i];
+            }
+            printf("a menor idade e %d", menor_idade);
+        } else if (opcao == 3) {
+            int alunos18 = 0;
+            for (int i = 0; i < 5; i++)
+            if (idade[i] >= 18)
+            alunos18 = idade[i];
+            printf("o numero de alunos com idade superior ou igual a 18 e de: %d", alunos18);
+        }else if (opcao == 4) {
+            for (int i = 0; i < 5; i++)
+                for (int z = 0; z < 5; z++) {
+                    printf("idade: %d, aluno %d\n", idade [i], z);
+}}
+    } while (opcao != 0);
+
     return 0;
 }
