9f9f9d98ff75b7e14f2574e9e45c647cfa21fb15
Joel <a12022@ae4outubro.pt>
2024-11-23 21:25:41 +0000

    I started but didn't finish doing exercise two (I'm very tired).

---
+++ NOVEMBER/23 NOV/Group one (Simple sequential program)/Exercise two.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char toupper(char resp); // Mudar
+
+int main() {
+    // Fazer um programa em C que imprime uma tabela com a tabuada de 1 a 9
+
+    /* Organizar (Ver o 'final project')
+    int opcao = 0;
+      int numero = 0;
+          char resp;
+
+      do {
+          printf("Tabuada do um: ");
+          scanf("%d", &opcao);
+          scanf("%d", &numero);
+
+          if (opcao == 1) {
+              for (int i = 1; i <= 10; i++)
+                  printf("1 x %d = %d\n", i, i);
+          } else if (opcao == 2) {
+              for (int i = 1; i <= 10; i++)
+                  printf("2 x %d = %d\n", i, i);
+          }
+      }
+    while (toupper(resp) == 'S'); // Mudar
+      return 0;
+      */
+}
