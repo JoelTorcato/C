08153d731732f9366bb31cb5199c43fdf7461f3c
JoelFernandes <a12022@ae4outubro.pt>
2024-12-05 13:32:06 +0000

    new commit

--- DECEMBER/03 DEC/exercise five.c
+++ DECEMBER/03 DEC/exercise five.c
@@ -23,7 +23,35 @@ int main() {
     }
     for (int i = 0; i < frase[i]; i++) {
         if (frase[i] == '\0');
-        printf("%c", frase[i]);
+        printf("%c ", frase[i]);
     }
+
+    char palavra[100];
+
+    printf("\nIntroduza uma palavra: ");
+    scanf("%s", palavra);
+
+    printf("A sua palavra ao contrário: %s\n", strrev(palavra));
+
+
+    char segundaPalavra[100];
+    char vogais[2] = "aeiouAEIOU"; // Adicionar para maiúsculas também
+    int contador = 0;
+
+    printf("Insira uma palavra: ");
+    // scanf("%s", segundaPalavra);
+    gets(segundaPalavra);
+
+
+    for (int i = 0; i < strlen(segundaPalavra); i++) { // Contar o número de carateres que a palavra tem
+        for (int j = 0; j < strlen(vogais); j++) { // Contar o número de vogais
+            if (segundaPalavra[i] == vogais[j]) {
+                contador++;
+            }
+        }
+    }
+    printf("\nA palavra informada possui %d vogais\n", contador);
+
+    return 0;
 
 }
