d4e3f7357b0ea56aa98bd036c0363a51729265a6
Joel <a12022@ae4outubro.pt>
2024-12-06 12:46:55 +0000

    I put my code in clean code

--- DECEMBER/03 DEC/Exercise five (Clean Code).c
+++ DECEMBER/03 DEC/Exercise five (Clean Code).c
@@ -2,110 +2,115 @@
 #include <string.h>
 #include <ctype.h>
 
-// Escopo global (Reutilizáveis)
+// Global scope (Reusable)
 
-void getNome(char *nome) {
+void getName(char *name) {
     printf("Insira o seu nome: ");
-    fgets(nome, 100, stdin); // fgets inclui vereficação de limite de buffer, e o gets não
-    nome[strcspn(nome, "\n")] = '\0'; // Usamos para evitar problemas com o \n no final
+    fgets(name, 100, stdin); // fgets inclui vereficação de limite de buffer, e o gets não
+    name[strcspn(name, "\n")] = '\0'; // Usamos para evitar problemas com o \n no final
 }
 
-void printNomeVertical(char *nome) {
-    for (int i = 0; nome[i]; i++) {
+void printNameVertical(char *name) {
+    for (int i = 0; name[i]; i++) {
         // nome [i] = nome[i] != '\0'
-        printf("%c\n", nome[i]);
+        printf("%c\n", name[i]);
     }
 }
 
-void printNomeHorizontalUppercase(char *nome) {
-    for (int i = 0; nome[i]; i++) {
+void printNameHorizontalUppercase(char *name) {
+    for (int i = 0; name[i]; i++) {
         // Ver como funciona melhor o loop
-        printf("%c ", toupper(nome[i]));
+        printf("%c ", toupper(name[i]));
     }
     printf("\n");
 }
 
-void printPalavraReversa(char *palavra) {
-    int len = strlen(palavra); // len de strlen
+void printWordReversed(char *word) {
+    int length = strlen(word); // len de strlen
     printf("A sua palavra ao contrário: ");
-    for (int i = len - 1; i >= 0; i--) {
+    for (int i = length - 1; i >= 0; i--) {
         // Fazemos o loop de trás para a frente (Ver melhor 'int i = len - 1')
-        printf("%c", palavra[i]);
+        printf("%c", word[i]);
     }
     printf("\n");
 }
 
-int contarVogais(char *segundaPalavra) {
-    char vogais[] = "aeiouAEIOU";
-    int contador = 0;
-    for (int i = 0; i < strlen(segundaPalavra); i++) {
+int countVowels(char *word) {
+    char vowels[] = "aeiouAEIOU";
+    int count = 0;
+    for (int i = 0; i < strlen(word); i++) {
         // 'i < palavra[i]' compara o índice i com o valor ASCII do caractere
-        for (int j = 0; j < strlen(vogais); j++) {
-            if (segundaPalavra[i] == vogais[j]) {
-                contador++;
+        for (int j = 0; j < strlen(vowels); j++) {
+            if (word[i] == vowels[j]) {
+                count++;
             }
         }
     }
-    return contador;
+    return count;
 }
 
-int contarAa(char *frase) {
-    char Aa[] = "Aa";
-    int contadorAa = 0;
-    for (int i = 0; i < strlen(frase); i++) {
-        for (int j = 0; j < strlen(Aa); j++) {
-            if (frase[i] == Aa[j]) {
-                contadorAa++;
+int countA(char *phrase) {
+    char aChars[] = "Aa";
+    int count = 0;
+    for (int i = 0; i < strlen(phrase); i++) {
+        for (int j = 0; j < strlen(aChars); j++) {
+            if (phrase[i] == aChars[j]) {
+                count++;
             }
         }
     }
-    return contadorAa;
+    return count;
 }
 
-// Escopo local (Não reutilizáveis)
-
-int main() {
-    char nome[100];
-    getNome(nome);
-
-    printf("O seu nome possui %d caracteres\n", (int) strlen(nome)); // '(int)strlen(nome)' = 'strlen(nome)'
-    printNomeVertical(nome);
-    printNomeHorizontalUppercase(nome);
-
-    char palavra[100];
-    printf("\nIntroduza uma palavra: ");
-    scanf("%s", palavra);
-    printPalavraReversa(palavra);
-
-    char segundaPalavra[100];
-    printf("\nIntroduza a segunda palavra: ");
-    scanf("%s", segundaPalavra);
-
-    int numVogais = contarVogais(segundaPalavra); // Contamos as vogais da segunda palavra
-    printf("A palavra informada possui %d vogais\n", numVogais);
-
-    char primeiraFrase[100];
-    printf("\nInsira uma frase: ");
-    scanf("%s", primeiraFrase);
-
-    int numAa = contarAa(primeiraFrase);
-    printf("A palavra informada possui %d Aa\n", numAa);
-
-
-    char numeroTelemovel[9];
-    printf("Insira o seu número de telemóvel: ");
-    scanf("%s", numeroTelemovel);
-
-    for (int i = 0; i < 1; i++)
-        if (numeroTelemovel[i] == '0') {
+void identifyOperator(char *phoneNumber) {
+    switch (phoneNumber[0]) {
+        case '0':
             printf("A sua operadora é a Vodafone\n");
-        } else if (numeroTelemovel[i] == '1') {
+            break;
+        case '1':
             printf("A sua operadora é NOS\n");
-        } else if (numeroTelemovel[i] == '2') {
+            break;
+        case '2':
             printf("A sua operadora é MEO\n");
-        } else {
+            break;
+        default:
             printf("Não tem operadora\n");
-        }
+            break;
+    }
+}
+
+// Local scope (Non-reusable)
+int main() {
+    char name[100];
+    getName(name);
+
+    printf("O seu nome possui %d caracteres\n", (int) strlen(name)); // '(int)strlen(nome)' = 'strlen(nome)'
+    printNameVertical(name);
+    printNameHorizontalUppercase(name);
+
+    char word[100];
+    printf("\nIntroduza uma palavra: ");
+    scanf("%s", word);
+    printWordReversed(word);
+
+    char secondWord[100];
+    printf("\nIntroduza a segunda palavra: ");
+    scanf("%s", secondWord);
+
+    int vowelCount = countVowels(secondWord); // Contamos as vogais da segunda palavra
+    printf("A palavra informada possui %d vogais\n", vowelCount);
+
+    char phrase[100];
+    printf("\nInsira uma frase: ");
+    scanf(" %[^\n]", phrase);
+
+    int aCount = countA(phrase);
+    printf("A palavra informada possui %d Aa\n", aCount);
+
+    char phoneNumber[9];
+    printf("Insira o seu número de telemóvel: ");
+    scanf("%s", phoneNumber);
+    identifyOperator(phoneNumber);
 
     return 0;
 }
