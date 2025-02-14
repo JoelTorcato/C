3aa207e9ff4a8aa085fd8490ee1e18719674a3f9
JoelFernandes <a12022@ae4outubro.pt>
2025-02-14 16:39:14 +0000

    Update Defining a Structure with Bit Fields

--- 2025/February/14 February/Defining a Structure with Bit Fields/Defining a Structure with Bit Fields.c
+++ 2025/February/14 February/Defining a Structure with Bit Fields/Defining a Structure with Bit Fields.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
     struct Estado {
-        unsigned int ligado : 1;
-        unsigned int modo : 2;
-        unsigned int nivel : 3;
+        unsigned int ligado : 1; // 1 bit
+        unsigned int modo : 2; // 2 bits
+        unsigned int nivel : 3; // 3 bits
 };
 
 int main() {
