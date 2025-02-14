22d82f32258728a964dec02d42ba1e93b0438117
JoelFernandes <a12022@ae4outubro.pt>
2025-02-14 16:09:07 +0000

    I've been studying conversions.

---
+++ 2025/February/14 February/Conversions/Conversions.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    int decimal = 25;
+
+    printf("Decimal: %d\n", decimal);
+    printf("Binário (manual: 11001\n)");
+    printf("Octal: %o\n", decimal);
+    printf("Hexadecimal: %x\n", decimal);
+}
